358b6e8b26f28fbca861acaa4d1b9ae1b6bdb46f
Shailesh <shaileshtripathi511@gmail.com>
2021-12-05 11:07:40 +0530

    Added alert feature

--- src/App.js
+++ src/App.js
@@ -8,26 +8,43 @@ import Alert from './components/Alert';
 function App() {
  const [mode, setMode] = useState('light')
  const [btnText, setbtnText] = useState('Enable dark mode')
+ const [alert, setAlert] = useState(null)
  
+const showAlert=(message,type)=>{
+   setAlert({
+    msg:message,
+    type:type
+   })
+
+   setTimeout(()=>
+   {
+     setAlert(null)
+   },1000)
+   
+}
+
 const toggleMode=()=>{
   if(mode==='light')
   {
     setMode('dark');
     document.body.style.backgroundColor='rgb(35 49 72)'
     setbtnText('Enable light mode')
+    showAlert("Dark mode has been enabled","success")
   }
   else
   {
     setMode('light')
     document.body.style.backgroundColor='white'
     setbtnText('Enable dark mode')
+    showAlert("light mode has been enabled","success")
   }
 }
+
 
   return (
     <>
    <Navbar mode={mode} toggleMode={toggleMode} btnText={btnText}/>
-   
+   <Alert alert={alert}/>
    <Textfield mode={mode} toggleMode={toggleMode}/>
 
    </>
--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,12 +1,21 @@
-/* import React from 'react'
+ import React from 'react'
 
-export default function Alert() {
+export default function Alert(props) {
+
+  const capitalize=(word)=>{
+     var str=word.toLowerCase();
+     return str.charAt(0).toUpperCase()+str.slice(1);
+  }
+
     return (
-        <div className="alert alert-warning alert-dismissible fade show" role="alert">
-      <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-     <button type="button" className="close .alert-dismissible" data-dismiss="alert" aria-label="Close">
-     <span aria-hidden="true">&times;</span>
-    </button>
+      <div style={{height:"50px"}}>
+    {
+    props.alert && 
+    <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+    <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
+  
     </div>
+     }
+  </div>
     )
-} */
+} 
